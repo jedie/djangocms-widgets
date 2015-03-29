995f10a48fea797c31e400ac7a15567cbcb5faf7
JensDiemer <git@jensdiemer.de>
2015-03-29 12:35:47 +0200

    update README, release as v0.2.0

--- djangocms_widgets/__init__.py
+++ djangocms_widgets/__init__.py
@@ -9,7 +9,7 @@
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
-__version__ = (0,1,0,"beta")
+__version__ = (0,2,0)
 VERSION_STRING = '.'.join(str(part) for part in __version__)
 
 if __name__ == "__main__":
